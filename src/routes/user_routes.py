acc0008793627cbb2536587cef0c21d0ad77ec61
Li Haiyun <li_haiyun@nyp.edu.sg>
2025-07-16 14:25:01 +0800

    add validation using marshmallow

--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,15 +1,25 @@
 from flask import Blueprint, request, jsonify
+from marshmallow import ValidationError
 from src.models.user import User
+from src.schemas.user_schema import UserSchema
 
 bp = Blueprint("users", __name__, url_prefix="/users")
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
 
 @bp.route("/", methods=["GET"])
 def list_users():
     users = User.objects()
-    return jsonify([u.to_json() for u in users])
+    return jsonify(users_schema.dump(users))
 
 @bp.route("/", methods=["POST"])
 def create_user():
-    data = request.get_json()
-    user = User(**data).save()
-    return jsonify(user.to_json()), 201
+    try:
+        # Validate input data using schema
+        data = user_schema.load(request.get_json())
+        user = User(**data).save()
+        return jsonify(user_schema.dump(user)), 201
+    except ValidationError as err:
+        return jsonify({"errors": err.messages}), 400
+    except Exception as err:
+        return jsonify({"error": str(err)}), 500
